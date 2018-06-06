e509f89b84501022cec55a70e5e121b777643cf9
tokorom <tokorom@gmail.com>
2018-06-06 13:07:40 +0900

    Support xcassets

---
+++ app/code/SimpleSampleViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SimpleSampleViewController.swift
+//
+//  Created by ToKoRo on 2018-06-06.
+//
+
+import UIKit
+import Lottie
+
+class SimpleSampleViewController: UIViewController {
+    @IBOutlet weak var animationArea: UIView?
+
+    weak var animationView: LOTAnimationView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupAnimation()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        animationView?.play()
+    }
+
+    private func setupAnimation() {
+        guard let view = animationArea else {
+            return
+        }
+
+        guard let path = Bundle.main.path(forResource: "yes", ofType: "json") else {
+            return
+        }
+
+        // let animationView = LOTAnimationView(name: "yes")
+        // animationView.frame = view.bounds
+
+        let animationView = LOTAnimationView(filePath: path)
+        animationView.frame = animationView.sceneModel?.compBounds ?? view.bounds
+
+        // animationView.loopAnimation = loop
+        view.addSubview(animationView)
+        self.animationView = animationView
+    }
+}
